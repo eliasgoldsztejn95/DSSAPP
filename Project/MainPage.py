9340510767abe7cb00eb01f74012ab5f0cc3de2b
Elias <75029654+eliasgoldsztejn95@users.noreply.github.com>
2023-08-13 14:24:19 +0300

    Update MainPage.py

--- Project/MainPage.py
+++ Project/MainPage.py
@@ -1,10 +1,5 @@
 # Contents of ~/my_app/main_page.py
 import streamlit as st
-import datetime
-import random
-import pandas
-import openpyxl
-from datetime import datetime, timedelta
 
 st.markdown("# Main page ")
 st.sidebar.markdown("# Main page ")
